8697a5ad67b528458849683c2c74463fb901d75a
F4RR311 <valery.leo2016@yandex.ru>
2022-07-19 00:01:03 +0300

    refactor3

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,9 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true });
 
-app.use(cors);
+app.use(cors({
+  credentials: true,
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
