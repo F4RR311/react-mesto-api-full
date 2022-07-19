8059e5b64dbd56cd6d6b54250f6d978431e9d9a5
F4RR311 <valery.leo2016@yandex.ru>
2022-07-19 19:57:35 +0300

    try-fix6

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,28 +1,28 @@
-// const allowedCors = [
-//   'http://domainname.students.nomoredomains.sbs',
-//   'http://domainname.students.nomoredomains.sbs',
-//   'https://api.mymesto.nomoredomains.xyz',
-//   'http://api.mymesto.nomoredomains.xyz',
-//   'localhost:3000',
-//   'http://localhost:3000',
-// ];
-//
-// module.exports = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-//
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.status(200).end();
-//   }
-//
-//   next();
-// };
+const allowedCors = [
+  'http://domainname.students.nomoredomains.sbs',
+  'http://domainname.students.nomoredomains.sbs',
+  'https://api.mymesto.nomoredomains.xyz',
+  'http://api.mymesto.nomoredomains.xyz',
+  'localhost:3000',
+  'http://localhost:3000',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).end();
+  }
+
+  next();
+};
