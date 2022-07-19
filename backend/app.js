55e229d2a77f31f3619fb3bbb3deb611f7abc57e
F4RR311 <valery.leo2016@yandex.ru>
2022-07-19 19:50:49 +0300

    try-fix6

--- backend/app.js
+++ backend/app.js
@@ -10,17 +10,32 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const ErrorNotFound = require('./errors/ErrorNotFound');
-const cors = require('./middlewares/cors');
+//const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
+const cors= require('cors');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true });
 
+const options = {
+origin: [
+   'localhost:3000',
+  'http://localhost:3000',
+  'localhost:3001',
+  'http://localhost:3001',
+  'https://localhost:3001',
+  'domainname.students.nomoredomains.sbs',
+  'http://domainname.students.nomoredomains.sbs',
+  'https://domainname.students.nomoredomains.sbs',
+  'https://api.mymesto.nomoredomains.xyz',
+  'http://api.mymesto.nomoredomains.xyz',
+],
+  credentials:true,
+}
 
-app.use(cors);
+app.use('*', cors(options));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
